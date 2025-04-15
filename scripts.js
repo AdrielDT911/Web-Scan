8ef63c727f53a96d550c7c5e0cb840bfdf63e16d
Adriel Duarte <138633258+AdrielDT911@users.noreply.github.com>
2025-04-15 09:57:29 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ function abrirCamara() {
     // Inicia el escáner de QR
     html5QrCode.start(
         { facingMode: "environment" },
-        { fps: 10, qrbox: { width: 250, height: 250 } },
+        { fps: 10, qrbox: { width: 200, height: 200 } },
         (decodedText, decodedResult) => {
             console.log("QR detectado:", decodedText);
             procesarQr(decodedText, html5QrCode);
